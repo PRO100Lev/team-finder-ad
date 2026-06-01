61d258f0721f58b37908beed26a8e38316b6b363
Баталин Лев <lev.bat@bk.ru>
2026-06-01 13:22:59 +0500

    2 попытка

--- projects/admin.py
+++ projects/admin.py
@@ -11,7 +11,7 @@ class SkillAdmin(admin.ModelAdmin):
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ["id", "name", "owner", "status", "created_at"]
     search_fields = ["name", "description"]
     list_filter = ["status"]
     readonly_fields = ["created_at"]
+    list_display = ["id", "name", "owner", "status", "created_at"]
